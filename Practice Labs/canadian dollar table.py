c0a357f0fffcd227581acfb16da10075cd498f82
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2024-02-13 15:49:46 -0700

    -

--- Practice Labs/canadian dollar table.py
+++ Practice Labs/canadian dollar table.py
@@ -8,3 +8,11 @@ print(format("====", ">10s"), format("====", ">10s"))
 print(format("Yesterday", ">10s"), format(yesterday, "+10.4f"))
 print(format("Today", ">10s"), format(today, "+10.4f"))
 print(format("Change", ">10s"), format(change, "+10.4f"))
+
+print("\n\n\n")
+#print output aligned to 2 columns using format specifiers
+print("{:>10s} {:>10s}".format("Date", "Rate"))
+print("{:>10s} {:>10s}".format("====", "===="))
+print("{:>10s} {:>+10.4f}".format("Yesterday", yesterday))
+print("{:>10s} {:>+10.4f}".format("Today", today))
+print("{:>10s} {:>+10.4f}".format("Change", change))
