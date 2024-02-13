9bb1e7190248433204c15b7c27a868e8a54c7b5a
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2024-02-13 16:00:43 -0700

    -

--- Practice Labs/grocery_list.py
+++ Practice Labs/grocery_list.py
@@ -3,4 +3,5 @@ item = input("Enter grocery item ('quit’ to quit):")
 while item != "quit":
     gorcery.append(item)
     item = input("Next item: ")
+print("Here is your grocery list:")
 print(gorcery)
