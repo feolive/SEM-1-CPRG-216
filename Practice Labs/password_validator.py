893d8fbfbd0be39d6e0cc6e07e7a832c8094563d
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2024-02-13 15:31:29 -0700

    password-validator

--- Practice Labs/password_validator.py
+++ Practice Labs/password_validator.py
@@ -4,23 +4,23 @@
 #check for at least 1 uppercase letter
 #check for at least 1 lowercase letter
 #output valid or invalid message
-
-password = input("Enter a password: ")
 valid = False
-if len(password) >=8:                                                                               
-   digit_count = 0
-   upper_count = 0
-   lower_count = 0
-   for char in password:
-       if char.isdigit():
-           digit_count += 1
-       if char.isupper():
-           upper_count += 1
-       if char.islower():
-           lower_count += 1
-   if digit_count >= 1 and upper_count >= 1 and lower_count >= 1:
-       valid = True      
-if valid:
-    print("Passowrd is valid")
-else:
-    print("Password is invalid")
+while not valid:
+    password = input("Enter a password: ")
+    if len(password) >=8:                                                                               
+        digit_count = 0
+        upper_count = 0
+        lower_count = 0
+        for char in password:
+            if char.isdigit():
+                digit_count += 1
+            if char.isupper():
+                upper_count += 1
+            if char.islower():
+                lower_count += 1
+        if digit_count >= 1 and upper_count >= 1 and lower_count >= 1:
+            valid = True      
+    if valid:
+        print("Passowrd is valid")
+    else:
+        print("Password is invalid, it should be at least 8 characters long, contain at least 1 number, 1 uppercase letter and 1 lowercase letter. Try again.")
